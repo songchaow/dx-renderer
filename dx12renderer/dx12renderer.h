1738f58536c2105d7fb3b478915c04e989292d50
Songchao Wang <songchaow@outlook.com>
2021-03-30 20:46:41 +0800

    Move some codes from main into renderer.cpp

--- dx12renderer/dx12renderer.h
+++ dx12renderer/dx12renderer.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "resource.h"
+
+void RenderFrame();
+void RenderUI();
+bool d3dbootstrap(HWND hwindow);
---
+++ dx12renderer/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "stdafx.h"
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+extern HINSTANCE hInst;                                // current instance
+extern HWND hWindow;                                   // current window handle
+extern WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+extern WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+extern WNDCLASSEXW windowClass;
